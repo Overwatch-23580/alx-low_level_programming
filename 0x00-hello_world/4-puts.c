cc4e89b921831670afc5f0702e2322e2dc007a73
Nkurunziza Jean Yves <nkurunzizabreezzy@gmail.com>
2023-02-16 15:00:33 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,8 @@
 /**
  * main - Entry point
  */
- int main(void)
+int main(void)
 {
-	puts("\"Programming is likebuilding a multilingual puzzle\"");
+	puts("\"Programming is likebuilding a multilingual puzzle");
 	return (0);
 }
