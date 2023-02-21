e7d7f5fbad313213a37a8150a84b0de3d0598a14
Nkurunziza Jean Yves <nkurunzizabreezzy@gmail.com>
2023-02-21 15:39:54 -0800

    Respected Betty

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * main -Entry point
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: Always returns 0
+ *
  */
 
 int main(void)
