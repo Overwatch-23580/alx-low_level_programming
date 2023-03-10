c2dc03bb30f30bfb348191dd013df3e637c61f49
Nkurunziza Jean Yves <nkurunzizabreezzy@gmail.com>
2023-03-10 14:36:27 -0800

    fixed 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,57 +2,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+
 #define UNUSED(x) (void)(x)
+
 /**
- * StringCheck - checks string
+ * StringCheck - checks if a string is a valid number
  * @s: string to check
- * Return: boolean
+ *
+ * Return: 1 if string is a valid number, 0 otherwise
  */
 int StringCheck(char *s)
 {
-	int i = 0;
+    int i;
 
-	for (; s[i] != '\0'; i++)
-	{
-		if (!isdigit(s[i]))
-		{
-			return (0);
-		}
-	}
-	return (1);
+    for (i = 0; s[i] != '\0'; i++)
+    {
+        if (!isdigit(s[i]))
+        {
+            return (0);
+        }
+    }
+
+    return (1);
 }
+
 /**
  * main - main function
- * @argc: argumentc
- * @argv: vector of arguments
- *Return: always 0
+ * @argc: argument count
+ * @argv: array of arguments
+ *
+ * Return: 0 on success, 1 on error
  */
-int main(int argc, char  *argv[])
+int main(int argc, char *argv[])
 {
-	int i;
-	int result = 0;
+    int i;
+    int result = 0;
 
-	if (argc > 1)
-	{
-		for (i = 1; i < argc; i++)
-		{
-			if (StringCheck(argv[i]))
-			{
-				result += atoi(argv[i]);
-			}
-			else
-			{
-				printf("Error\n");
-				return (1);
-			}
-		}
-		printf("%d\n", result);
-		return (0);
-	}
-	else
-	{
-		printf("%d\n", 0);
-		return (1);
-	}
+    if (argc > 1)
+    {
+        for (i = 1; i < argc; i++)
+        {
+            if (StringCheck(argv[i]))
+            {
+                result += atoi(argv[i]);
+            }
+            else
+            {
+                printf("Error\n");
+                return (1);
+            }
+        }
 
+        printf("%d\n", result);
+        return (0);
+    }
+    else
+    {
+        printf("%d\n", 0);
+        return (1);
+    }
 }
+
